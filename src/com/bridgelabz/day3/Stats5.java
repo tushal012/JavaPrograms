868e7fe1e7db8214c86b8fdf3326e4e8172fb337
tushal kumar <tushalkashyap11@gmail.com>
2021-07-29 23:41:57 -0700

    Added: java day 3 programs

---
+++ src/com/bridgelabz/day3/Stats5.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.day3;
+
+public class Stats5 {
+    public static void main(String[] args) {
+        double[] array = new double[5];
+        double avg,min,max;
+        for (int i=0;i<5;i++){
+            int num = (int) Math.floor(Math.random() * 10 ) % 2;
+            array[i]+=num;
+        }
+        System.out.println();
+        System.out.print("Array Value : ");
+        for (int j=0;j<5;j++){
+            System.out.print(" "+array[j]);
+        }
+    }
+}
