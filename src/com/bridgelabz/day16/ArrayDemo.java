f0e853bdb179319daf88b2311e98a91df8eba85f
tushal kumar <tushalkashyap11@gmail.com>
2021-08-31 02:17:38 -0700

    Added: day 16 programs added

--- src/com/bridgelabz/day16/ArrayDemo.java
+++ src/com/bridgelabz/day16/ArrayDemo.java
@@ -1,2 +1,122 @@
-package com.bridgelabz.day16;public class ArrayDemo {
+package com.bridgelabz.day16;
+
+import com.bridgelabz.util.Utility;
+
+import java.io.PrintWriter;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+
+        Utility u=new Utility();
+        System.out.println("Enter number of rows: ");
+        int m = u.inputInteger();
+        System.out.println("Enter number of columns: ");
+        int n = u.inputInteger();
+
+        //calling methods
+        int [][] a  =arrayInt(m,n);
+        double [][] b  =arrayDouble(m,n);
+        String [][] c  =arrayBoolean(m,n);
+
+        //calling display method
+        display(a,b,c,m,n);
+
+    }
+
+    public static int[][] arrayInt(int m,int n)
+    {
+        int a[][]=new int[m][n];
+        Utility u=new Utility();
+        System.out.println();
+        System.out.println("Integer Array");
+
+        for(int i = 0; i<m; i++)
+        {
+            for (int j = 0; j<n; j++)
+            {
+                a[i][j] = u.inputInteger();
+            }
+        }
+        return a;
+    }
+
+    public static double[][] arrayDouble(int m,int n)
+    {
+        double b[][]=new double[m][n];
+        Utility u=new Utility();
+        System.out.println();
+        System.out.println("Double Array");
+
+        for(int i = 0; i<m; i++)
+        {
+            for (int j = 0; j<n; j++)
+            {
+                b[i][j] = u.inputDouble();
+            }
+        }
+        return b;
+    }
+
+    public static String[][] arrayBoolean(int m,int n)
+    {
+        String c[][]=new String[m][n];
+        Utility u=new Utility();
+        System.out.println();
+        System.out.println("Boolean Array");
+
+        for(int i = 0; i<m; i++)
+        {
+            for (int j = 0; j<n; j++)
+            {
+                c[i][j] = u.inputString();
+            }
+        }
+        return c;
+    }
+
+    //display method
+    public static void display(int[][]a,double[][]b,String[][]c,int m,int n)
+    {
+        PrintWriter pw=new PrintWriter(System.out,true);
+
+        //display integers
+        System.out.println();
+        pw.println("2D ARRAY INTEGER");
+
+        for(int i = 0; i<m; i++)
+        {
+            for (int j = 0; j<n; j++)
+            {
+                pw.print("\t"+a[i][j]+" ");
+            }
+            pw.println("\t");
+        }
+
+
+        //display double
+        System.out.println();
+        pw.println("2D ARRAY DOUBLE");
+
+        for(int i = 0; i<m; i++)
+        {
+            for (int j = 0; j<n; j++)
+            {
+                pw.print("\t"+b[i][j]+" ");
+            }
+            pw.println("\t");
+        }
+
+        //display boolean
+        System.out.println();
+        pw.println("2D ARRAY BOOLEAN");
+
+        for(int i = 0; i<m; i++)
+        {
+            for (int j = 0; j<n; j++)
+            {
+                pw.print("\t"+c[i][j]+" ");
+            }
+            pw.println("\t");
+        }
+    }
 }
--- src/com/bridgelabz/util/Utility.java
+++ src/com/bridgelabz/util/Utility.java
@@ -1,13 +1,48 @@
 package com.bridgelabz.util;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import com.bridgelabz.day15.UserDetails;
+
+import java.io.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Utility {
     private BufferedReader br;
+
+    private final String REGEX_NAME = "<<name>>";
+    private final String REGEX_FULLNAME = "<<full name>> ";
+    private final String REGEX_MOBILE_NO = "xxxxxxxxxx";
+    private final String REGEX_DATE = "12/06/2016";
+
+    //constructor to initialize bufferedReader
+    public Utility(){
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    //Regex pattern matcher match the string and replace the regex pattern with user details.
+    public String convertString(UserDetails userDetails, String message){
+        Pattern p = Pattern.compile(REGEX_NAME);
+        Matcher m = p.matcher(message);
+        message = m.replaceAll(userDetails.getfName());
+
+        p = Pattern.compile(REGEX_FULLNAME);
+        m = p.matcher(message);
+        message = m.replaceAll(userDetails.getfName()+" "+userDetails.getlName());
+
+        p = Pattern.compile(REGEX_MOBILE_NO);
+        m = p.matcher(message);
+        message = m.replaceAll(userDetails.mobileNo());
+
+        p = Pattern.compile(REGEX_DATE);
+        m = p.matcher(message);
+        message = m.replaceAll(userDetails.date());
+
+        return message;
+    }
 
     /**
      * Purpose: method for finding year is leap or not
@@ -312,6 +347,68 @@ public class Utility {
         return "";
     }
 
+    //Take Double Input
+    public double inputDouble(){
+        try{
+            try{
+                return Double.parseDouble(br.readLine());
+            }
+            catch(NumberFormatException nfe){
+                System.out.println(nfe.getMessage());
+            }
+        }catch(IOException ioe){
+            System.out.println(ioe.getMessage());
+        }
+        return 0.0;
+    }
+
+
+
+    //Function take String in dd/mm/yyyy format and return Date Object
+    public Date printDate(String date){
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy");
+        try{
+            return sdf.parse(date);
+        }
+        catch(ParseException pe){
+            return null;
+        }
+    }
+
+    //format date object in this format 01/12/2016
+    public String getFormatedDate(Date date){
+        SimpleDateFormat sdf=new SimpleDateFormat("dd/mm/yyyy");
+        return sdf.format(date);
+    }
+
+    //Take file name as input and return string of file text
+    public String getFileText(String fileName){
+        try{
+            br=new BufferedReader(new FileReader(fileName));
+            StringBuilder sb=new StringBuilder();
+            String line=br.readLine();
+            while(line!=null){
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line=br.readLine();
+            }
+            return sb.toString();
+        }
+        catch(Exception e){
+            return null;
+        }
+        finally{
+            try{
+                br.close();
+            }
+            catch(IOException ioe){
+            }
+        }
+    }
+
+    //this function take two parameter
+    // 1 what data to write.
+    // 2 file name
     public void writeToFile(String data,String fileName)throws Exception{
         File file = new File(fileName);
         if (!file.exists()) {
@@ -323,8 +420,6 @@ public class Utility {
         writer.flush();
         writer.close();
     }
-
-
 
 
 
