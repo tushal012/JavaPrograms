ffe72a04a514cfea76392da776a70682cd6e4f40
tushal kumar <tushalkashyap11@gmail.com>
2021-10-05 17:24:32 +0530

    Added: java day 23 programs

--- src/com/bridgelabz/day23/AnimalSound.java
+++ src/com/bridgelabz/day23/AnimalSound.java
@@ -1,2 +1,30 @@
-package com.bridgelabz.day23;public class AnimalSound {
+package com.bridgelabz.day23;
+
+import java.io.*;
+
+public class AnimalSound {
+    public static void verse(String animal, String noise) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter("animal.txt"));
+        BufferedReader br = new BufferedReader(new FileReader("animal.txt"));
+        bw.write("Old MacDonald had a farm\n");
+        bw.write("E-I-E-I-O\n");
+        bw.write("And on that farm he had a \n " + animal);
+        bw.write("E-I-E-I-O\n");
+        bw.write("With a " + noise + "-" + noise + " here\n ");
+        bw.write("And a " + noise + "-" + noise + " there\n ");
+        bw.write("Here a " + noise + ", there a \n " + noise );
+        bw.write( "Everywhere a \n " + noise + "-" + noise );
+        bw.write( "Old MacDonald had a farm\n" );
+        bw.write( "E-I-E-I-O\n" );
+
+        bw.close();
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        verse( "cat" , "meow" );
+        verse( "duck" , "quack" );
+
+    }
 }
--- src/com/bridgelabz/day23/JavaIO.java
+++ src/com/bridgelabz/day23/JavaIO.java
@@ -1,2 +1,26 @@
-package com.bridgelabz.day23;public class JavaIO {
+package com.bridgelabz.day23;
+
+import java.io.*;
+
+public class JavaIO {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("Test.txt"));
+        BufferedWriter bw = new BufferedWriter(new FileWriter("Test.txt"));
+        bw.write("Avinash\n");
+        bw.write("Atul\n");
+        bw.write("Tushal\n");
+        bw.write("Sairam\n");
+
+        String name;
+        StringBuffer sb = new StringBuffer();
+        while ((name = br.readLine()) != null) {
+            name = name.replace("Sairam", "monika");
+            System.out.println(name);
+            sb.append(name);
+        }
+        br.close();
+        bw.write(sb.toString(), 0, sb.length());
+        bw.close();
+
+    }
 }
