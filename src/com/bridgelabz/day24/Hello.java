431bea7a8ac286f2e5480032899ea27f89a69598
tushal kumar <tushalkashyap11@gmail.com>
2021-10-19 08:04:15 +0530

    message

--- src/com/bridgelabz/day24/Hello.java
+++ src/com/bridgelabz/day24/Hello.java
@@ -3,4 +3,5 @@ package com.bridgelabz.day24;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello java");
+        System.out.println("world");
     }}
