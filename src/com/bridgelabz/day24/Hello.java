7f4ce91927e291d8d84c9d8291e545424a671d30
tushal kumar <tushalkashyap11@gmail.com>
2021-10-19 09:56:09 +0530

    Added : feature branch

--- src/com/bridgelabz/day24/Hello.java
+++ src/com/bridgelabz/day24/Hello.java
@@ -3,6 +3,6 @@ package com.bridgelabz.day24;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello java");
-        System.out.println("world");
+        System.out.println("merged");
 
     }}
